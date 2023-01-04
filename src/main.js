4e0b788acb881e89f4c3e665c0a3159b3903f4be
Эрнест <101067653+Nexoner@users.noreply.github.com>
2023-01-04 12:29:26 +0300

    1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import SortViewer from './view/sorting-template.js';
 import FilterViewer from './view/filters-template.js';
 import FormEditor from './view/edit-form-template.js';
 import PointsList from './view/points-list.js';
+import Point from './view/point.js';
 import {render} from './render.js';
 
 const mainElement = document.querySelector('.page-body');
@@ -16,3 +17,7 @@ render(new SortViewer(), headerElement);
 render(new FormEditor(), headerElement);
 render(new FilterViewer(), filtersElement);
 render(new PointsList(), headerElement);
+
+const hiddenPoint = document.querySelectorAll('.trip-events__item');
+
+render (new Point(), hiddenPoint[1]);
