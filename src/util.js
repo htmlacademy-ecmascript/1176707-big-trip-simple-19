f84d411b67df52a1b5efd314dfde66c60bc761a1
Эрнест <101067653+Nexoner@users.noreply.github.com>
2023-01-17 23:13:27 +0300

    1

--- src/util.js
+++ src/util.js
@@ -1,13 +1,24 @@
 import dayjs from 'dayjs';
 
 const DATE_FORMAT = 'DD-MM-YY HH:mm';
+const TIME_FORMAT = 'HH:mm';
 
 function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
-function humanizePointDueDate(dueDate) {
+function humanizePointDate(dueDate) {
   return dueDate ? dayjs(dueDate).format(DATE_FORMAT) : '';
 }
 
-export {getRandomArrayElement, humanizePointDueDate};
+function humanizePointTime(dueDate) {
+  return dueDate ? dayjs(dueDate).format(TIME_FORMAT) : '';
+}
+
+function getRandomDate() {
+  const date = new Date();
+
+  date.setFullYear(Math.random(2000, 2500));
+}
+
+export {getRandomArrayElement, humanizePointDate, humanizePointTime};
--- src/view/point-view.js
+++ src/view/point-view.js
@@ -1,49 +1,58 @@
 import {createElement} from '../render.js';
+import { humanizePointDate, humanizePointTime } from '../util.js';
 
-function addPoint() {
+
+function addPoint(point) {
+  const { typePoint, dueDate, price, offers} = point;
+
   return (
-    ` <li class="trip-events__item">
+    `<li class="trip-events__item">
         <div class="event">
-        <time class="event__date" datetime="2019-03-18">MAR 18</time>
-          <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/flight.png" alt="Event type icon">
-          </div>
-          <h3 class="event__title">Flight Chamonix</h3>
-          <div class="event__schedule">
-          <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T12:25">12:25</time>
+        <time class="event__date" datetime="${dueDate.number}">${humanizePointDate(dueDate.number)}</time>
+        <div class="event__type">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/flight.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">${typePoint}</h3>
+        <div class="event__schedule">
+        <p class="event__time">
+            <time class="event__start-time" datetime="${dueDate.start}">${humanizePointTime(dueDate.start)}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T13:35">13:35</time>
-          </p>
-          </div>
+            <time class="event__end-time" datetime="${dueDate.end}">${humanizePointTime(dueDate.start)}</time>
+        </p>
+        </div>
             <p class="event__price">
             &euro;&nbsp;
-              <span class="event__price-value">160</span>
+            <span class="event__price-value">${price}</span>
             </p>
-          <h4 class="visually-hidden">Offers:</h4>
-          <ul class="event__selected-offers">
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
             <li class="event__offer">
-            <span class="event__offer-title">Add luggage</span>
-              &plus;&euro;&nbsp;
-            <span class="event__offer-price">50</span>
+            <span class="event__offer-title">${offers.offer1.description}</span>
+            &plus;&euro;&nbsp;
+            <span class="event__offer-price">${offers.offer1.price}</span>
             </li>
             <li class="event__offer">
-            <span class="event__offer-title">Switch to comfort</span>
-              &plus;&euro;&nbsp;
-            <span class="event__offer-price">80</span>
+            <span class="event__offer-title">${offers.offer2.description}</span>
+            &plus;&euro;&nbsp;
+            <span class="event__offer-price">${offers.offer2.price}</span>
             </li>
-          </ul>
-          <button class="event__rollup-btn" type="button">
-          <span class="visually-hidden">Open event</span>
-          </button>
-      </div>
+        </ul>
+        <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+        </button>
+    </div>
     </li>`
   );
 }
 
 export default class PointView {
+
+  constructor(point) {
+    this.point = point;
+  }
+
   getTemplate() {
-    return addPoint();
+    return addPoint(this.point);
   }
 
   getElement() {
