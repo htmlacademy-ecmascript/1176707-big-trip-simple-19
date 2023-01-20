374d4081c990996d179c05cc1416f199e853442d
Эрнест <101067653+Nexoner@users.noreply.github.com>
2023-01-20 20:39:23 +0300

    1

--- src/mock/point.js
+++ src/mock/point.js
@@ -4,37 +4,39 @@ import { TYPE , COUNTRY , OFFERS, POINT_COUNTS } from '../const.js';
 const mockPoints = [
 ];
 
+createMockPoints(POINT_COUNTS);
+
 function createMockPoints(count) {
   createObjects(count);
 }
-
-createMockPoints(POINT_COUNTS);
 
 function createObjects(count) {
   for (let i = 0; i <= count; i++) {
     const point = {};
     const offers = {};
 
-    createPoint(point, i);
-    createOffers(offers, point);
+    point[i] = createPoint(i);
+    createOffers(offers, point , i);
+    point[i].offers = offers;
 
-    mockPoints.push(point);
+    mockPoints.push(point[i]);
   }
 }
 
-function createPoint(point, i) {
-  point['type'] = `${getRandomArrayElement(TYPE)}`;
-  point['date_from'] = new Date(generateRandomDate(new Date(2022, 0, 12), new Date(), 0, 12));
-  point['date_to'] = new Date(generateRandomDate(new Date(2020, 12, 24), new Date(), 12, 24));
-  point['base_price'] = getRandomNumber(2000);
-  point['destination'] = getRandomArrayElement(COUNTRY);
-  point['id'] = i;
+function createPoint(i) {
+  return {
+    type: `${getRandomArrayElement(TYPE)}`,
+    'date_from': new Date(generateRandomDate(new Date(2022, 0, 12), new Date(), 0, 12)),
+    'date_to': new Date(generateRandomDate(new Date(2020, 12, 24), new Date(), 12, 24)),
+    'base_price': getRandomNumber(2000),
+    destination: getRandomArrayElement(COUNTRY),
+    id: i
+  };
 }
 
-function createOffers(offers, point) {
+function createOffers(offers,) {
   for (let b = 0; b < 3; b++) {
     offers['offer'[b]] = getRandomArrayElement(OFFERS);
-    point['offers'] = offers;
   }
 }
 
